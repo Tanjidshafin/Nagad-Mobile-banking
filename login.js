d0cdedbfb515d05cad71f010b0c63751506349c8
Tanjid Karim <you@example.com>
2024-09-18 22:29:45 +0600

    Login Page Done

---
+++ login.js
@@ -0,0 +1,19 @@
+const loginNumber = document.getElementById("login_number")
+const loginPassword = document.getElementById("login_password")
+document.getElementById("passwordShow").addEventListener("click", function () {
+    if (loginPassword.type == "password") {
+        loginPassword.type = "text"
+    }
+    else {
+        loginPassword.type = "password"
+    }
+})
+document.getElementById("login_btn").addEventListener("click", function (event) {
+    event.preventDefault()
+    if (loginNumber.value == "01715257080" && loginPassword.value == "1234k") {
+        window.location.href = "main.html"
+    }
+    else {
+        alert("Wrong Number. Try again")
+    }
+})
